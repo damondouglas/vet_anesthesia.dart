1e397333d0e173aa6bd6de44d299182123714779
Damon Douglas <douglas.damon@gmail.com>
2013-07-05 21:42:00 -0400

    fixed scrolling, finalize med data

--- web/lib/xtable.dart
+++ web/lib/xtable.dart
@@ -2,7 +2,9 @@ import 'package:web_ui/web_ui.dart';
 import 'dart:html';
 
 class Table extends WebComponent {
-
+  @observable
+  String refpointid = "";
+  
   Map _medication = 
     {
      "PRE OP":
@@ -48,6 +50,104 @@ class Table extends WebComponent {
        "l":_dosebased(,),
        "h":_dosebased(,)
        }*/
+     },
+     "POST OP":
+     {
+       "Rimadyl":
+       {
+       "l":_dosebased(4.0,50.0),
+       "h":_dosebased(4.0,50.0)
+       }
+     },
+     "AB":
+     {
+       "Cefazolin":
+       {
+       "l":_dosebased(22.0,100.0),
+       "h":_dosebased(22.0,100.0)
+       },
+       "Ampicillin":
+       {
+       "l":_dosebased(20.0,250.0),
+       "h":_dosebased(20.0,250.0)
+       }
+     },
+     "INDUCE":
+     {
+       "Propofol":
+       {
+       "l":_dosebased(4.0,10.0),
+       "h":_dosebased(6.0,10.0)
+       }
+     },
+     "IVF":
+     {
+       "CAT 1st hour":
+       {
+       "l":_dosebased(5.0,1.0),
+       "h":_dosebased(5.0,1.0)
+       },
+       "DOG 1st hour":
+       {
+       "l":_dosebased(10.0,1.0),
+       "h":_dosebased(10.0,1.0)
+       }
+     },
+     "BLOCK":
+     {
+       "Bupivacaine":
+       {
+       "l":_dosebased(1.0,5.0),
+       "h":_dosebased(2.0,5.0)
+       },
+       "Lidocaine":
+       {
+       "l":_dosebased(1.0,20.0),
+       "h":_dosebased(2.0,20.0)
+       }
+     },
+     "911":
+     {
+       "Atipamazole":
+       {
+       "l":_dosebased(0.012,5.0),
+       "h":_dosebased(0.021,5.0)
+       },
+       "Atropine":
+       {
+       "l":_dosebased(0.02,0.54),
+       "h":_dosebased(0.04,0.54)
+       },
+       "Glycopyrrolate":
+       {
+       "l":_dosebased(0.01,0.2),
+       "h":_dosebased(0.01,0.2)
+       },
+       "Epinephrine":
+       {
+       "l":_dosebased(0.1,1.0),
+       "h":_dosebased(0.2,1.0)
+       },
+       "Lidocaine CAT":
+       {
+       "l":_dosebased(0.25,20.0),
+       "h":_dosebased(0.5,20.0)
+       },
+       "Lidocaine DOG":
+       {
+       "l":_dosebased(2.0,20.0),
+       "h":_dosebased(4.0,20.0)
+       },
+       "Ephedrine":
+       {
+       "l":_dosebased(0.1,5.0),
+       "h":_dosebased(0.2,5.0)
+       },
+       "Hetastarch":
+       {
+       "l":_dosebased(2.5,1.0),
+       "h":_dosebased(5.0,1.0)
+       }
      }
     };
   void inserted() {
@@ -83,11 +183,21 @@ class Table extends WebComponent {
       paint();
     });
     
+    window.onScroll.listen((_){
+      /*
+      var y = window.scrollY;
+      var xtable = query("div[is='x-table']");
+      var t = xtable.offsetHeight - y;
+      xtable.style.top = "${t}px";
+      */
+    });
+    
   }
   
   void paint(){
     var xtable = query("div[is='x-table']");
-    var previous = xtable.previousElementSibling;
+    var previous = query("$refpointid");
+
     if(previous != null) {
       var height = previous.offsetHeight;
       xtable.style.top = "${height}px";
